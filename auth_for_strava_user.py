8cb94bd0923f7c8070438cd9cd2617fb27607d08
G0erman <goerman.rafi@gmail.com>
2022-08-03 17:20:27 -0500

    Add typer and parameters to generate tocken

--- auth_for_strava_user.py
+++ auth_for_strava_user.py
@@ -1,30 +1,46 @@
-import requests
+"""
+Generate strava token for each team member, it is necessary provide the strava_id and autorization code.
+
+$ python auth_for_strava_user.py strava_team_member_id strava_team_member_code
+"""
+
 import json
+import os
+import requests
+import typer
 
-# Make Strava auth API call with your 
-# client_code, client_secret and code
-response = requests.post(
-    url='https://www.strava.com/oauth/token',
-    data={
-                            'client_id': [CLIENT_ID],
-                            'client_secret': '[CLIENT_SECRET]',
-                            'code': '[CODE]',
-                            'grant_type': 'authorization_code'
-                            }
-                )
 
-# Save json response as a variable
-strava_tokens = response.json()
-# Save tokens to file
-with open('strava_tokens_[STRAVA_ID].json', 'w') as outfile:
-    json.dump(strava_tokens, outfile)
+def generate_token(strava_id: int, code: str):
+    client_id = int(os.getenv('CLIENT_ID'))
+    client_secret = str(os.getenv('CLIENT_SECRET'))
 
-print(outfile)
+    # Make Strava auth API call with your
+    # client_code, client_secret and code
+    response = requests.post(
+        url='https://www.strava.com/oauth/token',
+        data={
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'code': code,
+            'grant_type': 'authorization_code'
+        }
+    )
 
-# Open JSON file and print the file contents 
-# to check it's worked properly
-with open('strava_tokens_[STRAVA_ID].json') as check:
-    data = json.load(check)
+    # Save json response as a variable
+    strava_tokens = response.json()
+    # Save tokens to file
+    with open(f'strava_tokens_{strava_id}.json', 'w') as outfile:
+        json.dump(strava_tokens, outfile)
 
-print(data)
+    print(outfile)
 
+    # Open JSON file and print the file contents
+    # to check it's worked properly
+    with open(f'strava_tokens_{strava_id}.json') as check:
+        data = json.load(check)
+
+    print(data)
+
+
+if __name__ == "__main__":
+    typer.run(generate_token)
