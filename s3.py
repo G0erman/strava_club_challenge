eded4cc74b87cfd86971a73069205c911e8db19d
G0erman <goerman.rafi@gmail.com>
2023-05-14 18:53:25 -0500

    Added feature to upload data to a public or private bucket.

--- s3.py
+++ s3.py
@@ -5,7 +5,14 @@ from constans import DATA_FILENAME
 BUCKET = "pystravans"
 KEY = "front/data.csv"
 REGION = "us-east-2"
+ARGUMENTS = {'ACL': 'public-read'}
 
-def upload_file_to_s3():
+def upload_file_to_s3(is_public=False):
     S3 = boto3.client('s3', region_name=REGION)
-    S3.upload_file(DATA_FILENAME, BUCKET, KEY)
+    try:
+        if is_public:
+            S3.upload_file(DATA_FILENAME, BUCKET, KEY, ExtraArgs=ARGUMENTS)
+        else:
+            S3.upload_file(DATA_FILENAME, BUCKET, KEY)
+    except Exception as e:
+        print("Something unexpected happened!", e)
